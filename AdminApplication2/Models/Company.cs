093fe1f79df4d901f8df440b78a098933cd21492
Polclard <alenjang007@gmail.com>
2025-01-16 18:55:47 +0100

    Add project files.

---
+++ AdminApplication2/Models/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Domain_Models
+{
+    public class Company
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? CompanyName { get; set; }
+        public string? CompanyAddress { get; set;}
+        public virtual ICollection<Employee>? ListOfEmployees { get; set; } = new List<Employee>();
+    }
+}
---
+++ AdminApplication2/Models/Polyclinic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Domain_Models
+{
+    public class Polyclinic
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? PolyclinicName { get; set; }
+        public string? PolyclinicAddress { get; set; }
+        public int? Reputation {  get; set; }
+        public int AvailableSlots { get; set; }
+        public virtual ICollection<HealthExamination> HealthExaminations { get; set; } = new List<HealthExamination>();
+    }
+}
